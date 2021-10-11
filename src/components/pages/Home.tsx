2980e3c8cf2a369472942a09f0ad923baf06b3b1
Jéssica Aquiles <73039194+JEZIGA01@users.noreply.github.com>
2021-10-10 21:07:11 -0300

    Update Home.tsx

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -10,7 +10,7 @@ function Home() {
 
       <h1>A configurar o Webpack + Typescript + Babel </h1>
       <h2>App desenvolvido por:Jéssica Aquiles</h2>
-        <a href="https://www.linkedin.com/in/jessicaaquiles/" target="_blank"  rel="noreferrer">  
+        <a href="https://www.linkedin.com/in/jessicaaquiles/" target="_blank"  rel="noreferrer" </a>  
           <img  alt="logo" style={{ width: 400 }} src={logo}  /> 
       <Footer />
     </>
